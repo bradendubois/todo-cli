53e7f5f598821a931fb620017173323673e3e354
Braden Dubois <drab3n@gmail.com>
2020-07-19 23:34:31 -0600

    Basic "add" shell added, with low/medium/high priority flags

--- functions/add.sh
+++ functions/add.sh
@@ -1,6 +1,37 @@
 #!/usr/bin/bash
 
 add() {
+
     echo "Add called"
-    echo "$*"
+
+    PRIORITY=""
+
+    # Parse whether a priority was given
+    case $1 in
+
+        # Low priority
+        -l | --low )                PRIORITY="LOW"
+                                    shift
+                                    ;;
+        
+        # Medium priority
+        -m | --medium )             PRIORITY="MEDIUM"
+                                    shift
+                                    ;;
+        
+        # High priority
+        -h | --high )               PRIORITY="HIGH"
+                                    shift
+                                    ;;
+
+    esac
+
+    # Relay priority parsed
+    if [ "${PRIORITY}" != "" ]; then
+        echo "Priority: ${PRIORITY}"
+    fi
+
+    # Message that would be added
+    echo "Message: $*"
+
 }
